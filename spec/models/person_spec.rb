f98c0ba7239ef0a1fb2f00a08d441884538f83e8
jonathan <jonathanbd1985@gmail.com>
2014-12-04 13:24:34 +0000

    generando telefonos y asociandolos a personas

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -8,4 +8,7 @@ RSpec.describe Person, :type => :model do
 class Person < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
 end
+  it 'has an array of phone numbers' do
+     expect(person.phone_numbers).to eq([])
+  end
 end
