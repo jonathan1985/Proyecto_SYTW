7851bdd8e9b7794adbfac16cb9b53750e0ff11b3
jonathan <jonathanbd1985@gmail.com>
2014-12-05 11:09:46 +0000

    incrustando pruebas

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,2 +1,10 @@
 class PhoneNumber < ActiveRecord::Base
+
+let(:phone_number) { PhoneNumber.new(number: "1112223333") }
+it 'must have a reference to a person' do
+  phone_number.person_id = nil
+  expect(phone_number).not_to be_valid
+end
+  belongs_to :person
+
 end
--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid' do
+    expect(phone_number).to be_valid
+  end
+ let(:phone_number) { PhoneNumber.new(number: "1112223333", person_id: 1) }
+ it 'is associated with a person' do
+  expect(phone_number).to respond_to(:person)
+ end
 end
